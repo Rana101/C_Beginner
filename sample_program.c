fe4bf8759f8c4afdd06dc26b8096d88e1cd67814
“[Rajashekar <[raju10naidu@gmail.com]>
2021-09-26 22:26:03 +0530

    first code

---
+++ sample_program.c
@@ -0,0 +1,29 @@
+/*
+* Author:- Rana
+* Date:- 26 - september-2021
+* Description:- Basic C program Template                                  //documentation and comments section
+*/
+
+
+#include <stdio.h>                    //link section
+
+#define PI =3.14                      //definition section                    //link and defintion together called "Pre-processor" section
+
+float area;
+int a = 7;                        //global variable declaration section
+
+int main()                           // main function
+{
+    int b=10;                        //local variable declaration section
+
+    printf("hello, iam Rana\n");     // statement & expressions
+    return 0;
+}
+
+
+//add 2 numbers
+
+int add(int a, int b)             // subprogram section or user defined functions
+{
+return a+b;
+}
